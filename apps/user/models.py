d95cb88d2b4f8e07ef3ab8f62d9ff71530282c2f
krv006 <rvkamronbek@gmail.com>
2025-01-05 13:10:56 +0500

    model users

--- apps/user/models.py
+++ apps/user/models.py
@@ -3,3 +3,23 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+
+# class User(AbstractUser):
+#     class Type(TextChoices):
+#         OPERATOR = 'operator', 'Operator'
+#         ADMIN = 'admin_side', 'Admin_side'
+#         USER = 'user', 'User'
+#
+#     username = None
+#     first_name = None
+#     last_name = None
+#     email = EmailField(unique=True)
+#     name = CharField(max_length=255)
+#     is_active = BooleanField(default=False)
+#     type = CharField(max_length=25, choices=Type.choices, default=Type.USER, verbose_name="user type")
+#
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+#     objects = CustomUserManager()
